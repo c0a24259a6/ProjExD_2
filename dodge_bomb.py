5fdcbaa0449d1ae205357d14e0f683a6fb0b4c57
Naoki Moriyama <c0a24259a6@edu.teu.ac.jp>
2025-09-30 14:38:44 +0900

    練習3:こうかとんと爆弾を画面内に閉じ込める

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,6 +11,19 @@ DELTA = {
             pg.K_LEFT: (-5, 0),
             pg.K_RIGHT: (5, 0),
         }
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect or ばくだんRect
+    戻り値：判定結果タプル（横方向, 縦方向）
+    画面内ならTrue, 画面外ならFalse
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向にはみ出ていたら
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向にはみ出ていたら
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -27,6 +40,7 @@ def main():
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
     vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -50,8 +64,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横にはみ出ていたら
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
